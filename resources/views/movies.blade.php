edf6d0cb8322e811be69f95ad15ba75cb0aef76c
AlessioAbbati <alex_117@live.it>
2023-06-26 16:56:00 +0200

    add images with foreach and mod db

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -8,7 +8,7 @@
             <div class="col">
                 
                 <div class="card" style="width: 18rem;">
-                    {{-- <img src="{{ Vite::asset('/resources/img/' . $movie->img) }}" class="card-img-top" alt=""> --}}
+                    <img src="{{ Vite::asset('/resources/img/' . $movie->img) }}" class="card-img-top" alt="">
                     <div class="card-body">
                         <h5 class="card-title">{{ $movie->title }}</h5>
                         <p class="card-text">{{ $movie->date }}</p>
