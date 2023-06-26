2258a2880fd3ab96ee9286842fe551806e9fee3d
AlessioAbbati <alex_117@live.it>
2023-06-26 18:39:07 +0200

    mod header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,12 @@
 <header>
 
-    <div class="container">
-        <h1 class="text-center">Top Rated Movies</h1>
+    <div class="container d-flex justify-content-between">
+        <h3 class="text-center text-danger">Boolean Movies</h3>
+        <ul class="d-flex">
+            <li><a href="">Home</a></li>
+            <li><a href="">Features</a></li>
+            <li><a href="">Contacts</a></li>
+        </ul>
     </div>
     
 </header>
